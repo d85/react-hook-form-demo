576b3521e276a3c652d1f7abb43beb02e1b005bc
Dan Carlier <dan.carlier@gmail.com>
2023-05-18 10:03:42 +1000

    Add pattern validation for email field

--- src/components/YouTubeForm.tsx
+++ src/components/YouTubeForm.tsx
@@ -25,11 +25,20 @@ export const YouTubeForm = () => {
         <label htmlFor="username">Username</label>
         <input type="text" id="username" {
           ...register("username", {
-          required: "Username is required",
+          required: {
+            value: true,
+            message: "Username is required",
+          }
         })} />
 
         <label htmlFor="email">Email</label>
-        <input type="email" id="email" {...register("email")} />
+        <input type="email" id="email" {...register("email",{
+          pattern: {
+            value: 
+            /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+            message: "Invalid email format"
+          }
+        })} />
 
         <label htmlFor="channel">Channel</label>
         <input type="text" id="channel" {...register("channel", {
