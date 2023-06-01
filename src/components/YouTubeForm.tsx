1deac7e2047c0d68362d66a65a70a7730c9331e8
Dan Carlier <dan.carlier@gmail.com>
2023-06-01 19:16:52 +1000

    Add mandatory validation to all fields

--- src/components/YouTubeForm.tsx
+++ src/components/YouTubeForm.tsx
@@ -53,56 +53,88 @@ export const YouTubeForm = () => {
 
         <div className="form-control">
           <label htmlFor="email">Email</label>
-          <input type="email" id="email" {...register("email", {
-            pattern: {
-              value:
-                /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
-              message: "Invalid email format"
-            },
-            validate: {
-              notAdmin: (fieldValue) => {
-                return (
-                  fieldValue !== "admin@example.com" ||
-                  "Enter a different email address"
-                )
+          <input type="email" id="email" {...register("email",
+            {
+              required: {
+                value: true,
+                message: "Email is required",
               },
-              notBlackListed: (fieldValue) => {
-                return (
-                  !fieldValue.endsWith("baddomain.com") ||
-                  "This domain is not supported"
-                )
+              pattern: {
+                value:
+                  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                message: "Invalid email format"
+              },
+              validate: {
+                notAdmin: (fieldValue) => {
+                  return (
+                    fieldValue !== "admin@example.com" ||
+                    "Enter a different email address"
+                  )
+                },
+                notBlackListed: (fieldValue) => {
+                  return (
+                    !fieldValue.endsWith("baddomain.com") ||
+                    "This domain is not supported"
+                  )
+                }
               }
-            }
-          })} />
+            })} />
           <p className="error">{errors.email?.message}</p>
         </div>
 
         <div className="form-control">
           <label htmlFor="channel">Channel</label>
           <input type="text" id="channel" {...register("channel", {
-            required: "Channel is required"
+            required: {
+              value: true,
+              message: "Channel is required",
+            }
           })} />
           <p className="error">{errors.channel?.message}</p>
         </div>
 
         <div className="form-control">
           <label htmlFor="twitter">Twitter</label>
-          <input type="text" id="twitter" {...register("social.twitter")} />
+          <input type="text" id="twitter" {...register("social.twitter", {
+            required: {
+              value: true,
+              message: "Twitter is required",
+            }
+          })} />
+          <p className="error">{errors.social?.twitter?.message}</p>
         </div>
 
         <div className="form-control">
           <label htmlFor="facebook">Facebook</label>
-          <input type="text" id="facebook" {...register("social.facebook")} />
+          <input type="text" id="facebook" {...register("social.facebook", {
+            required: {
+              value: true,
+              message: "Facebook is required",
+            }
+          })} />
+          <p className="error">{errors.social?.facebook?.message}</p>
         </div>
 
         <div className="form-control">
           <label htmlFor="primary-phone">Primary phone number</label>
-          <input type="text" id="primary-phone" {...register("phoneNumbers.0")} />
+          <input type="text" id="primary-phone" {...register("phoneNumbers.0", {
+            required: {
+              value: true,
+              message: "Primary phone number is required",
+            }
+          })} />
+          <p className="error">{errors.phoneNumbers && errors.phoneNumbers[0]?.message}</p>
         </div>
 
         <div className="form-control">
           <label htmlFor="secondary-phone">Secondary phone number</label>
-          <input type="text" id="secondary-phone" {...register("phoneNumbers.1")} />
+          <input type="text" id="secondary-phone" {...register("phoneNumbers.1", {
+            required: {
+              value: true,
+              message: "Secondary phone number is required",
+            }
+          })} />
+          <p className="error">{errors.phoneNumbers && errors.phoneNumbers[1]?.message}</p>
         </div>
 
         <button>Submit</button>
